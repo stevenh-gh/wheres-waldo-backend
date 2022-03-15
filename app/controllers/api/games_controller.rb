da740453945debb6f9d5f9989306772f3a6b3ae8
steven <steven.h@live.com>
2022-03-14 22:09:53 -0700

    add games#show action

--- app/controllers/api/games_controller.rb
+++ app/controllers/api/games_controller.rb
@@ -3,4 +3,9 @@ class Api::GamesController < ApplicationController
 		@games=Game.all
 		render :json => @games.map {|game| game.as_json.merge({img: url_for(game.img)})}
 	end
+
+	def show
+		@game=Game.find(params[:id])
+		render :json => @game.as_json.merge({img: url_for(@game.img)})
+	end
 end
